095de66401519382c8e6afc17c45762d27d2e613
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 19:00:54 +0200

    done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,35 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	return 0;
+int main() {
+    const int arrSize = 7;  
+    int sourceArr[arrSize] = { 1, 2, 3, 4, 5, 6, 7};  
+    int destinationArr[arrSize]; 
+
+   
+    int* sourcePtr = sourceArr;
+    int* destinationPtr = destinationArr;
+
+   
+    for (int i = 0; i < arrSize; ++i) {
+        *destinationPtr = *sourcePtr;  
+        
+        ++sourcePtr;  
+        ++destinationPtr;  
+    }
+
+
+   cout << "Output array: ";
+    for (int i = 0; i < arrSize; ++i) {
+      cout << sourceArr[i] << " ";
+    }
+
+    cout << "\nCopied array: ";
+    for (int i = 0; i < arrSize; ++i) {
+        cout << destinationArr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
-```
