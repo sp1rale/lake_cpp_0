150b8f63cfda7f85decf700b6addbbeb439b716d
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 19:39:05 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	const int arrSize = 7;
+	int sourceArr[arrSize] = { 1, 2, 3, 4, 5, 6, 7 };
+	int destArr[arrSize];
+
+	int *sourcePtr = sourceArr;
+	int *destPtr = destArr + arrSize - 1;
+
+	for (int i = 0; i < arrSize; ++i)
+	{
+		*destPtr = *sourcePtr;
+		++sourcePtr;
+		--destPtr;
+	}
+
+	for (int i = 0; i < arrSize; ++i){
+	cout << sourceArr[i] << " ";
+	}
+	
+    for (int i = 0; i < arrSize; ++i)
+	{
+	cout << destArr[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
-```
