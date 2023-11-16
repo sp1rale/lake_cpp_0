f0214cf19584bef86b9ccd0cf6ab8b35c81c8abf
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 17:12:16 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	int a, b;
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+
+	int  *ptrA = &a;
+	int  *ptrB = &b;
+
+	if (*ptrA > *ptrB)
+	{
+		cout << "The largest number - " << *ptrA << endl;
+	}
+	else if (*ptrA > *ptrB)
+	{
+		cout << "The largest number - " << *ptrB << endl;
+	}
+	else
+	{
+		cout << "equal number"   << endl;
+	}
 	return 0;
 }
-```
