a9c834a7d87a2b9ac9bcbba2e65b663cda342e4b
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 17:33:29 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    int a, b;
+
+    cout << "Enter two values: ";
+    if (cin >> a >> b)
+    
+    {
+        cout << "The original values: a = " << a << ", b = " << b << endl;
+        int tmp = a;
+        a = b;
+        b = tmp;
+        cout << "After swapping: a = " << a << ", b = " << b << endl;
+    }
+    else
+    {
+       cout << "Unexpected error occured" << endl;
+    }
 	return 0;
 }
-```
