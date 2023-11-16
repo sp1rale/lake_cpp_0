937242b250ff18d6c35f7389f2fc9ea3ddb8d9d0
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 18:20:03 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	const int arrSIZE = 7;
+	int arr[arrSIZE] = { 1, 2, 3, 4, 5, 6, 7 };
+
+	int *ptr = arr;
+
+	int sum = 0;
+	for (int i = 0; i < arrSIZE; i++)
+	{
+		sum += *ptr;
+		++ptr;
+	}
+
+	cout << "Sum of array elements: " << sum << endl;
+
 	return 0;
 }
-```
