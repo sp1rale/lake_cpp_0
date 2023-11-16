228d870e1675572d3e40365cd7e2da19f0094941
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 17:55:18 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,40 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	double operat1, operat2;
+	char operation;
+
+	cout << "Enter the first number: ";
+	cin >> operat1;
+
+	cout <<"Choose the operation (+, -, *, /): ";
+	cin >> operation;
+
+	cout << "Enter the second number: ";
+	cin >> operat2;
+
+	double *ptrOperat1 = &operat1;
+	double *ptrOperat2 = &operat2;
+
+	double  result;
+
+	switch (operation)
+	{
+		case '+':
+			cout << *ptrOperat1 << " + " << *ptrOperat2 << " = " << *ptrOperat1 + *ptrOperat2 << endl;
+			break;
+		case '-':
+			cout << *ptrOperat1 << " - " << *ptrOperat2 << " = " << *ptrOperat1 - *ptrOperat2 << endl;
+			break;
+		case '*':
+			cout << *ptrOperat1 << " * " << *ptrOperat2 << " = " << *ptrOperat1 * *ptrOperat2 << endl;
+			break;
+		case '/':
+			cout << *ptrOperat1 << " / " << *ptrOperat2 << " = " << *ptrOperat1 / *ptrOperat2 << endl;
+			break;
+		default:
+			cout << "Error! The operator is not correct";
+			break;
+	}
 	return 0;
 }
-```
