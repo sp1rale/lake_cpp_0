cdf6ed6f82b09ebca30fc87ef74fd1dc64bed40c
sp1rale <dimayaremchyk24@gmail.com>
2023-11-16 19:27:44 +0200

    done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	const int arrSIZE = 7;
+	int numbers[arrSIZE] = { 1, 2, 3, 4, 5, 6, 7 };
+	int* ptr = numbers;
+
+	cout << "Output array: ";
+	for (int i = 0; i < arrSIZE; ++i) {
+		cout << *(ptr + i) << " ";
+	}
+	int* startPtr = ptr;
+	int* endPtr = ptr + arrSIZE - 1;
+
+	while (startPtr < endPtr) {
+		int temp = *startPtr;
+		*startPtr = *endPtr;
+		*endPtr = temp;
+		
+		++startPtr;
+		--endPtr;
+	}
+
+	cout << "\nArray after reordering: ";
+	for (int i = 0; i < arrSIZE; ++i) {
+		cout << *(ptr + i) << " ";
+	}
+cout << endl;
 	return 0;
 }
-```
